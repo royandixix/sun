5d5c8a3f1bfbc3ae805bfceb456486716d998fc5
royandi <randiroyandi@gmail.com>
2025-09-29 21:46:46 +0800

    fix: correct case-sensitive import for Portofolio

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Poppins } from "next/font/google";
 import "./globals.css";
+
 import Navbar from "./home/components/Navbar/Navbar";
 
 // Geist font (asli)
